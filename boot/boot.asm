6a27cbcfd1b280230d3a0adc656230e4d3d01627
arsen duisek Fullstack - all web languages and c++ java kotlin <arsenduisek11@gmail.com>
2026-06-10 17:06:26 +0500

    Fix boot.asm: remove incorrect high address calculations and properly initialize RSP for 64-bit mode

--- boot/boot.asm
+++ boot/boot.asm
@@ -10,17 +10,9 @@ align 4
 
 section .bss
 align 4096
-bootstrap_page_table:
-    resb 4096
-bootstrap_page_dir:
-    resb 4096
-bootstrap_pdpt:
-    resb 4096
-bootstrap_pml4:
-    resb 4096
-
 bootstrap_stack:
     resb 16384
+bootstrap_stack_top:
 
 section .text
 global _start
@@ -29,43 +21,13 @@ extern kernel_main
 _start:
     cli
     
-    mov esp, bootstrap_stack + 16384
+    mov rsp, bootstrap_stack_top
     
-    mov eax, bootstrap_pml4 - 0xC0000000
-    mov cr3, eax
+    mov rdi, rax
+    mov rsi, rbx
     
-    mov eax, cr4
-    or eax, 0x00000020
-    mov cr4, eax
+    call kernel_main
     
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 0x00000100
-    wrmsr
-    
-    mov eax, cr0
-    or eax, 0x80000001
-    mov cr0, eax
-    
-    lgdt [gdtr]
-    jmp 0x08:long_mode
-
-gdtr:
-    dw gdt_end - gdt - 1
-    dq gdt - 0xC0000000
-
-gdt:
-    dq 0x0000000000000000
-    dq 0x00209a0000000000
-    dq 0x0020920000000000
-    dq 0x00209a0000000000
-    dq 0x0020920000000000
-gdt_end:
-
-bits 64
-long_mode:
-    xor rax, rax
-    mov rax, kernel_main
-    call rax
     cli
     hlt
+    jmp $
